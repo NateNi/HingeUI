3a344e6fd44bcf8ed940a0b747e815943703475e
NateNi <nniesel90@gmail.com>
2025-02-09 15:09:32 -0600

    Updates

---
+++ precache-manifest.17ebd04190434f41edab020531f5e7e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42beda5de1a35f79504aeca0dec4e36a",
+    "url": "/HingeUI/index.html"
+  },
+  {
+    "revision": "d1e31a1d29350c1c7621",
+    "url": "/HingeUI/static/css/main.67cbaa02.chunk.css"
+  },
+  {
+    "revision": "ff1e86dded319e2c2e29",
+    "url": "/HingeUI/static/js/2.b9febd3f.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/HingeUI/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "d1e31a1d29350c1c7621",
+    "url": "/HingeUI/static/js/main.45eef5e3.chunk.js"
+  },
+  {
+    "revision": "644589f8d24d9de623be",
+    "url": "/HingeUI/static/js/runtime~main.0aee85b4.js"
+  },
+  {
+    "revision": "7ad3502e02d6981de752b2740f3c7b50",
+    "url": "/HingeUI/static/media/boy.7ad3502e.png"
+  },
+  {
+    "revision": "65e65c0399c691dc1737e1e65d0be57e",
+    "url": "/HingeUI/static/media/girl.65e65c03.png"
+  },
+  {
+    "revision": "b114b287c48f32785624549bd3f5a2a0",
+    "url": "/HingeUI/static/media/mini-heart.b114b287.svg"
+  }
+]);
